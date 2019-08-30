fbcc7ac0a34701cbff9a3eacebf0d34b475479e2
Margaret <msfinnan@gmail.com>
2019-08-30 15:01:40 -0700

    removed local history extension

--- .history/src/stores/CommentBoxStore_20190828100256.ts
+++
@@ -1,5 +0,0 @@
-import { observable } from "mobx";
-
-export class CommentBoxStore {
-    @observable public text: string = "(Optional) Provide details about your experience";
-}
--- .history/src/stores/CommentBoxStore_20190828101433.ts
+++
@@ -1,9 +0,0 @@
-import { observable } from "mobx";
-
-export class CommentBoxStore {
-    @observable public commentBoxText: string = "(Optional) Provide details about your experience";
-
-    @observable public commentBoxClassName: string = "experince-details-textbox";
-
-    @observable public commentBoxUserInput: string = "";
-}
