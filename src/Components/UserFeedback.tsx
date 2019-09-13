b3e3ac8bdf21261f5c94c1cff0bfac0116d655c9
Margaret <msfinnan@gmail.com>
2019-09-13 10:43:08 -0700

    Moved mehtods to fetch api data to FeedbackFormDataService.ts, incorperated other feedback from code review

--- src/Components/UserFeedback.tsx
+++ src/Components/UserFeedback.tsx
@@ -1,48 +1,71 @@
 import * as React from "react";
 import { observable } from "mobx";
-import { UserLength } from "../stores/FeedbackFormStore";
-// import {UserLength} from 
-
-// interface UserLength {
-//     username: string;
-//     length: number
-// }
+import { UserNumPosts } from "../stores/FeedbackFormStore";
 
 interface UserFeedbackProps {
     emoji: string
     satisfaction: string
     comments: string
-    apiData: UserLength[]
+    apiData: UserNumPosts[]
     // feedbackFormStore: FeedbackFormStore;
 }
 
 
 export class UserFeedback extends React.Component<UserFeedbackProps, {}>{
-    @observable private newArray: JSX.Element[];
 
+
+    // private timerID;
+    // @observable date: Date = new Date()
+
+    // private tick() {
+    //     this.setState({
+    //       date: new Date()
+    //     });
+    //   }
+
+    // public componentDidMount(){
+    //     console.log("in componentDidMount")
+    //     this.timerID = setInterval(
+    //         () => this.tick(),
+    //         1000
+    //       );
+
+    // }
+    // public getSnapshotBeforeUpdate(prevProps: UserFeedbackProps, prevState: {}) {
+    //     console.log("in getSnapShotBeforeUpdate prev props is ", prevProps, "prev state is ", prevState)
+    //     return null;
+    // }
     //noticed that you have to click submit twice for this to work now
     //when I comment out shouldComponentUpdate, it updates twice in the log, but only have to click submit once 
     public shouldComponentUpdate(nextProps: UserFeedbackProps, nextState: {}): boolean {
-        if (this.props.apiData == null) {
+        console.log("in shouldComponentUpdate")
+        if (nextProps.apiData == null) {
             return false;
         } else {
             return true;
         }
     }
-
+    
     //added componentDidUpdate with console.log to test shouldComponentUpdate. 
     //if shouldComponentUpdate always returns true, "component did update logs twice"
     //with the null check it only prints once, when this.props.apiData != null
-    componentDidUpdate() {
-        console.log("this.props.apiData is ", this.props.apiData, "component did update")
-    }
+    // public componentDidUpdate() {
+    //     console.log("in componentDidUpdate this.props.apiData is ", this.props.apiData)
+    // }
+
+    // public componentWillUnmount() {
+    //     alert("unmounting")
+    //     clearInterval(this.timerID);
+    // }
+
+    @observable private newArray: JSX.Element[];
 
     private showApiData = (): JSX.Element | JSX.Element[] => {
         if (this.props.apiData == null) {
             return <li>No API Data</li>
         } else {
             this.newArray = this.props.apiData.map((user, i) => {
-                return <li key={i}>{`${user.username} posted ${user.length} posts`}</li>
+                return <li key={i}>{`${user.username} posted ${user.numPosts} posts`}</li>
             })
             return <ul>{this.newArray}</ul>;
         }
